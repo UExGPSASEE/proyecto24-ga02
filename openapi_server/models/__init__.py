eb1900448677ee54a29941536cf735a14d1b27f2
Jesus Santiago <Sanjesvel@outlook.com>
2024-11-06 08:45:29 +0100

    Correcion_de_Informacion

--- openapi_server/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi_server.models.get_all_perfiles200_response_inner import GetAllPerfiles200ResponseInner
-from openapi_server.models.usuario import Usuario
-from openapi_server.models.usuario_perfiles_inner import UsuarioPerfilesInner
